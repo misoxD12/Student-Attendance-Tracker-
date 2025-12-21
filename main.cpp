ee5996a3060ed40f367f61c033db3b3dfe66a17a
MingSong <tohrucooldoragon@gmail.com>
2025-12-21 22:02:47 +0800

    rearranged here and there

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
-// *********************************************************
-// Program: AttendanceTracker.cpp
-// Course: CCP6114 Programming Fundamentals
-// Lecture Class: TC1L
-// Tutorial Class: TT4L
-// Trimester: 2530
-// Member_1:
-// Member_2: 
-// Member_3: 
-// Member_4: 
-// *********************************************************
-// Task Distribution
-// Member_1: Main Menu Loop, Initialize Sheet, Create Term, Count Rows
-// Member_2: Insert Row, Update Row
-// Member_3: View Sheet (CSV Format), Delete Row
-// Member_4: Define Columns, File I/O (Save to File, Read from File)
-// *********************************************************
-
-// ==========================================
-// SHARED GLOBAL VARIABLES & STRUCTURES
-// ==========================================
-
 #include <iostream>
 #include <string>
 #include <limits>
 
 using namespace std;
 
+
+//Constants 
 const int MAX_ROWS = 100;
 const int MAX_COLS = 10;
 
@@ -37,7 +17,7 @@ struct AttendanceRow {
 
 struct Column {
     string name;
-    string type; // "INT" or "TEXT"
+    string type; 
 };
 
 AttendanceRow sheet[MAX_ROWS];
@@ -47,134 +27,7 @@ int currentColCount = 0;
 
 string sheetName = "";
 
-void defineColumns(); 
-
-
-
-void initializeSheet() {
-    cout << "\n===========================================\n";
-    cout << "   STUDENT ATTENDANCE TRACKER - MILESTONE 1\n";
-    cout << "===========================================\n";
-    cout << "Enter attendance sheet name: ";
-
-    getline(cin, sheetName);
-    
-    cout << "Attendance sheet \"" << sheetName << "\" created successfully.\n";
-}
-
-void viewSheet() {
-
-    cout << endl;
-    cout << "-------------------------------------------" << endl;
-    cout << "View Attendance Sheet (CSV Mode)" << endl;
-    cout << "-------------------------------------------" <<endl;
-
-    // Print column headers
-    for (int i = 0; i < currentColCount; i++) {
-        cout << columns[i].name;
-        if (i < currentColCount - 1) cout << ", ";
-    }
-    cout << endl;
-
-    // Print attendance rows
-    for (int i = 0; i < currentRowCount; i++) {
-        cout << sheet[i].studentID << ", ";
-        cout << sheet[i].name << ", ";
-        cout << sheet[i].status << endl;
-    }
-}
-
-void insertRow() {
-    if (currentColCount == 0) {
-        cout << "Error: You must define columns (Option 1) before adding data." << endl;
-        return;
-    }
-
-
-    if (currentRowCount < MAX_ROWS) {
-        cout << "\n-------------------------------------------" << endl;
-        cout << "Insert New Attendance Row" << endl;
-        cout << "-------------------------------------------" << endl;
-
-
-        cout << "Enter " << columns[0].name << ": "; 
-        cin >> sheet[currentRowCount].studentID;
-
-   
-        while(cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid input. Please enter a number for " << columns[0].name << ": ";
-            cin >> sheet[currentRowCount].studentID;
-        }
-
-        cout << "Enter " << columns[1].name << ": ";
-        cin.ignore(); // Clear the buffer before getline
-        getline(cin, sheet[currentRowCount].name);
-      
-        cout << "Enter " << columns[2].name << ": ";
-        cin >> sheet[currentRowCount].status;
-
-        // Increment the counter
-        currentRowCount++;
-        cout << "Row inserted successfully." << endl;
-
-    } else {
-        cout << "Error: Attendance sheet is full!" << endl;
-    }
-}
-
-
-int main() {
-
-    int choice;
-
-    initializeSheet();
-
-    do {
-        cout << "\n-------------------------------------------";
-        cout << "\nMAIN MENU";
-        cout << "\n-------------------------------------------";
-        cout << "\n1. Define Columns";
-        cout << "\n2. Insert New Attendance Row";
-        cout << "\n3. View Attendance Sheet";
-        cout << "\n4. Exit";
-        cout << "\n\nEnter choice: ";
-        cin >> choice;
-
-        // Validation loop to handle non-integer inputs for menu
-        while(cin.fail()) {
-            cin.clear(); 
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-            cout << "Invalid input. Please enter a number (1-4): ";
-            cin >> choice;
-        }
-
-        switch (choice) {
-            case 1:
-                //defineColumns(); 
-                defineColumns();
-                break;
-            case 2:
-                insertRow();
-                cout << "2";   
-                break;
-            case 3:
-                //viewSheet(); 
-                viewSheet();  
-                break;
-            case 4:
-                cout << "\nExiting program...\n";
-                break;
-            default:
-                cout << "\nError: Invalid choice. Try again.\n";
-        }
-
-    } while (choice != 4);
-
-    
-    return 0;
-}
+//Functions 
 
 void defineColumns() { 
     // Ask for column count 
@@ -253,4 +106,130 @@ void defineColumns() {
     }
     cout << "\nSheet structure created successfully.\n";
 }
+
+
+void initializeSheet() {
+    cout << "\n===========================================\n";
+    cout << "   STUDENT ATTENDANCE TRACKER - MILESTONE 1\n";
+    cout << "===========================================\n";
+    cout << "Enter attendance sheet name: ";
+
+    getline(cin, sheetName);
+    
+    cout << "Attendance sheet \"" << sheetName << "\" created successfully.\n";
+}
+
+void viewSheet() {
+
+    cout << endl;
+    cout << "-------------------------------------------" << endl;
+    cout << "View Attendance Sheet (CSV Mode)" << endl;
+    cout << "-------------------------------------------" <<endl;
+
+    // Print column headers
+    for (int i = 0; i < currentColCount; i++) {
+        cout << columns[i].name;
+        if (i < currentColCount - 1) cout << ", ";
+    }
+    cout << endl;
+
+    // Print attendance rows
+    for (int i = 0; i < currentRowCount; i++) {
+        cout << sheet[i].studentID << ", ";
+        cout << sheet[i].name << ", ";
+        cout << sheet[i].status << endl;
+    }
+}
+
+void insertRow() {
+    if (currentColCount == 0) {
+        cout << "Error: You must define columns first before adding data." << endl;
+        return;
+    }
+
+
+    if (currentRowCount < MAX_ROWS) {
+        cout << "\n-------------------------------------------" << endl;
+        cout << "Insert New Attendance Row" << endl;
+        cout << "-------------------------------------------" << endl;
+
+
+        cout << "Enter " << columns[0].name << ": "; 
+        cin >> sheet[currentRowCount].studentID;
+
+   
+        while(cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Please enter a number for " << columns[0].name << ": ";
+            cin >> sheet[currentRowCount].studentID;
+        }
+
+        cout << "Enter " << columns[1].name << ": ";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the buffer before getline
+        getline(cin, sheet[currentRowCount].name);
+      
+        cout << "Enter " << columns[2].name << ": ";
+        cin >> sheet[currentRowCount].status;
+
+        // Increment the counter
+        currentRowCount++;
+        cout << "Row inserted successfully." << endl;
+
+    } else {
+        cout << "Error: Attendance sheet is full!" << endl;
+    }
+}
+
+
+int main() {
+
+    int choice;
+
+    initializeSheet();
+
+    //Main menu
+    do {
+        cout << "\n-------------------------------------------";
+        cout << "\nMAIN MENU";
+        cout << "\n-------------------------------------------";
+        cout << "\n1. Define Columns";
+        cout << "\n2. Insert New Attendance Row";
+        cout << "\n3. View Attendance Sheet";
+        cout << "\n4. Exit";
+        cout << "\n\nEnter choice: ";
+        cin >> choice;
+
+        // Validation loop to handle non-integer inputs for menu
+        while(cin.fail()) {
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cout << "Invalid input. Please enter a number (1-4): ";
+            cin >> choice;
+        }
+
+        switch (choice) {
+            case 1:
+                defineColumns();
+                break;
+            case 2:
+                insertRow(); 
+                break;
+            case 3:
+                viewSheet();  
+                break;
+            case 4:
+                cout << "\nExiting program...\n";
+                break;
+            default:
+                cout << "\nError: Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 4);
+
+    
+    return 0;
+}
+
+
 
